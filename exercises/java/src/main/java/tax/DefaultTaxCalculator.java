1d1b60e966355ce62cf6abab455cf615103a1c9e
Amy Hart <amy.hart@autotrader.co.uk>
2020-01-20 15:16:42 +0000

    Completed story two

--- exercises/java/src/main/java/tax/DefaultTaxCalculator.java
+++ exercises/java/src/main/java/tax/DefaultTaxCalculator.java
@@ -1,22 +1,17 @@
 package tax;
 
-import java.util.ArrayList;
-
 public class DefaultTaxCalculator extends TaxCalculator {
     @Override
     int calculateTax(Vehicle vehicle) {
         if(vehicle.getFuelType().equals(FuelType.PETROL)) {
             return calculatePetrolVehicleTax(vehicle);
+        } else if(vehicle.getFuelType().equals(FuelType.DIESEL)) {
+            return calculateDieselVehicleTax(vehicle);
         }
         return -1;
     }
 
-
-
-
     private int calculatePetrolVehicleTax(Vehicle vehicle) {
-
-
 
         if (vehicle.getCo2Emissions() > 255) {
             return 2070;
@@ -53,6 +48,47 @@ public class DefaultTaxCalculator extends TaxCalculator {
         }
         if (vehicle.getCo2Emissions() >= 1) {
             return 10;
+        }
+        return 0;
+    }
+
+    private int calculateDieselVehicleTax(Vehicle vehicle) {
+
+        if (vehicle.getCo2Emissions() > 255) {
+            return 2070;
+        }
+        if (vehicle.getCo2Emissions() >= 226) {
+            return 2070;
+        }
+        if (vehicle.getCo2Emissions() >= 191) {
+            return 1760;
+        }
+        if (vehicle.getCo2Emissions() >= 171) {
+            return 1240;
+        }
+        if (vehicle.getCo2Emissions() >= 151) {
+            return 830;
+        }
+        if (vehicle.getCo2Emissions() >= 131) {
+            return 515;
+        }
+        if (vehicle.getCo2Emissions() >= 111) {
+            return 205;
+        }
+        if (vehicle.getCo2Emissions() >= 101) {
+            return 165;
+        }
+        if (vehicle.getCo2Emissions() >= 91) {
+            return 145;
+        }
+        if (vehicle.getCo2Emissions() >= 76) {
+            return 125;
+        }
+        if (vehicle.getCo2Emissions() >= 51) {
+            return 105;
+        }
+        if (vehicle.getCo2Emissions() >= 1) {
+            return 25;
         }
         return 0;
     }
