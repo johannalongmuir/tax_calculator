1f17bc4bd6ce0ab5f9c2aeda2c2e1b59200b5ba2
Johanna Longmuir <johannalongmuir@gmail.com>
2020-01-21 10:17:49 +0000

    FeatureToggle add

---
+++ exercises/java/src/main/java/tax/FeatureToggle.java
@@ -0,0 +1,5 @@
+package tax;
+
+public class FeatureToggle {
+    private boolean FEATURE_FOUR_SECOND_TAX_PAYMENTS;
+}
--- exercises/java/src/test/java/tax/TaxCalculatorAfterFirstYearTest.java
+++ exercises/java/src/test/java/tax/TaxCalculatorAfterFirstYearTest.java
@@ -3,9 +3,7 @@ package tax;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-
 import java.time.LocalDate;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static java.time.Month.APRIL;
 import static tax.FuelType.*;
